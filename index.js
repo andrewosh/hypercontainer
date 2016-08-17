931cbdcf379b9c9159ed4a0e5bebc8876a685a1b
Andrew Osheroff <andrewosh@gmail.com>
2016-08-17 16:56:38 -0700

    radically changing the API to use copy-on-write

--- index.js
+++ index.js
@@ -1,114 +1,169 @@
-var mkdirp = require('mkdirp')
+var path = require('path')
+var proc = require('child_process')
+var Swarm = require('discovery-swarm')
+var Docker = require('dockerode')
+var hyperdrive = require('hyperdrive')
+var from = require('from2')
+var level = require('level')
+var cow = require('copy-on-write')
+var cuid = require('cuid')
 
-var conf = require('./conf')
-var DockerEngine = require('./lib/docker')
+var conf = require('../conf')
+var debug = require('debug')(conf.name)
 
-function Hypercontainer (db, opts) {
-  if (!(this instanceof Hypercontainer)) return new Hypercontainer(db, opts)
+function ImageManager (opts) {
+  if (!(this instanceof ImageManager)) return new ImageManager(opts)
   this.opts = opts || {}
-  this.db = db
-  this._initialized = false
+  this.db = opts.db || level(conf.db)
+  this.drive = hyperdrive(this.db)
 }
 
-Hypercontainer.prototype._initialize = function (cb) {
+ImageManager.prototype.import = function (name, cb) {
+  // Create an archive out of an Docker image tarball
+}
+
+ImageManager.prototype.list = function (cb) {
+  // List all available Hyperimages
+  throw Error('ImageManager.list is not implemented yet.')
+}
+
+function Hyperimage (name, archive, opts) {
+  if (!(this instanceof Hyperimage)) return new Hyperimage(name, opts)
+  this.opts = opts || {}
+  this.name = name
+  this.archive = archive
+}
+
+Hyperimage.prototype.run = function (opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts
+    opts = {}
+  }
   var self = this
-  mkdirp(conf.dbDir, function (err) {
-    if (err) return cb(err)
-    mkdirp(conf.containerDir, function (err) {
-      if (err) return cb(err)
-      self._initialized = true
-      cb()
+  var id = opts.id || cuid()
+  var mnt = opts.mnt || this.opts.mnt || path.join(conf.mnt, id)
+  if (!this.swarm) {
+    this.swarm = this.seed()
+  }
+
+  function createFileStream (entry, offset) {
+    var total = entry.length - offset
+    var cursor = self.archive.createByteCursor(entry.name, offset)
+    var read = 0
+    var done = false
+    // TODO: this can be optimized a lot
+    return from(function (size, next) {
+      if (done) return next(null, null)
+      cursor.next(function (err, buf) {
+        if (err) return next(err)
+        if (!buf) {
+          return next(null, null)
+        }
+        read += buf.length
+        if (read > total) {
+          done = true
+          var sliced = buf.slice(0, read - total)
+          return next(null, sliced)
+        }
+        return next(null, buf)
+      })
     })
+  }
+
+  function createIndexStream () {
+    return self.archive.list()
+  }
+
+  cow(mnt, {
+    dir: conf.store,
+    createFileStream: createFileStream,
+    createIndexStream: createIndexStream,
+    log: debug
+  }, function (err, fs) {
+    if (err) return cb(err)
+    debug('Filesystem mounted, booting container...')
+    fs.readdir('/', function (err, files) {
+      console.log('files are:', files)
+      if (err) throw err
+
+      files = files
+        .filter(function (file) {
+          return file !== '.' && file !== '..' && file !== 'proc' && file !== 'dev'
+        })
+        .map(function (file) {
+          return '-v ' + path.resolve(path.join(conf.containerDir, id, 'mnt', file)) + ':/' + file + ' '
+        })
+        .join('').trim().split(/\s+/)
+
+      var entrypoint = opts.cmd || '/bin/bash'
+
+      var command = ['run', '-it', '--entrypoint', entrypoint]
+      command = command.concat(['--net', opts.net || 'bridge'])
+        .concat(files).concat('tianon/true')
+      if (opts.env) {
+        var vars = [].concat(opts.env || [])
+        var env = []
+        vars.forEach(function (v) {
+          env.push('-e', v)
+        })
+        command = command.concat(env)
+      }
+      var spawn = function () {
+        proc.spawn('docker', command, {stdio: 'inherit'}).on('exit', function () {
+          console.log('EXITING')
+        })
+      }
+
+      var ns = new Buffer('nameserver 8.8.8.8\nnameserver 8.8.4.4\n')
+      fs.open('/etc/resolv.conf', 1, function (err, fd) {
+        if (err < 0) return spawn()
+        fs.write('/etc/resolv.conf', 0, ns.length, ns, fd, function (err) {
+          if (err < 0) return spawn()
+          fs.release('/etc/resolv.conf', fd, spawn)
+        })
+      })
+    })
+    return cb(null, Hypercontainer(id, self.archive, fs))
   })
 }
 
-Hypercontainer.prototype._getEngine = function (name) {
-  if (name === 'docker') {
-    return DockerEngine(this.db)
-  }
+Hyperimage.prototype.seed = function () {
+  var self = this
+  var swarm = Swarm()
+  swarm.listen()
+  swarm.join(this.archive.key)
+  swarm.on('connection', function (conn) {
+    conn.pipe(self.archive.replicate()).pipe(conn)
+  })
+  return swarm
 }
 
-Hypercontainer.prototype.run = function (engineName, image, opts, cb) {
+function Hypercontainer (id, archive, filesystem, opts) {
+  if (!(this instanceof Hypercontainer)) return new Hypercontainer(filesystem, opts)
+  this.opts = opts || {}
+  this.id = id
+  this.archive = archive
+  this.fs = filesystem
+}
+
+Hypercontainer.prototype.stop = function (cb) {
+  // Stop the container, unmount the filesystem, and delete the layers
+}
+
+Hypercontainer.prototype.commit = function (opts, cb) {
   if (typeof opts === 'function') {
     cb = opts
     opts = {}
   }
   var self = this
-  if (!this._initialized) {
-    this._initialize(_run)
-  }
-  function _run (err) {
-    if (err) return cb(err)
-    var engine = self._getEngine(engineName)
-    if (engine) {
-      return engine.run(image, opts, cb)
-    }
-    return cb(new Error('no engine found'))
-  }
-}
+  var name = opts.name || cuid()
 
-Hypercontainer.prototype.ps = function (engineName, opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
-  }
-  var self = this
-  if (!this._initialized) {
-    this._initialize(_ps)
-  } else {
-    return _ps()
-  }
-  function _ps (err) {
+  // Merge the filesystem's changes stream with the archive's index stream, then write
+  // a new archive (create a new Hyperimage) from the merged results
+  this.fs.createChangesStream(function (err, changesStream) {
     if (err) return cb(err)
-    var engine = self._getEngine(engineName)
-    if (engine) {
-      return engine.ps(opts, cb)
-    }
-    return cb(new Error('no engine found'))
-  }
-}
-
-Hypercontainer.prototype.create = function (engineName, id, opts, cb) {
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = {}
-  }
-  var self = this
-  if (!this._initialized) {
-    this._initialize(_create)
-  } else {
-    return _create()
-  }
-  function _create (err) {
-    if (err) return cb(err)
-    var engine = self._getEngine(engineName)
-    if (engine) {
-      return engine.create(id, opts, cb)
-    }
-    return cb(new Error('no engine found'))
-  }
-}
-
-Hypercontainer.prototype.run = function (engineName, image, opts, cb) {
-  console.log('in run')
-  if (typeof opts === 'function') {
-    cb = opts
-    opts = null
-  }
-  var self = this
-  if (!this._initialized) {
-    return this._initialize(_run)
-  } else {
-    return _run()
-  }
-  function _run (err) {
-    if (err) return cb(err)
-    var engine = self._getEngine(engineName)
-    if (engine) {
-      return engine.run(image, opts, cb)
-    }
-    return cb(new Error('no engine found'))
-  }
+    var originalStream = this.archive.list()
+  })
 }
 
 module.exports = Hypercontainer
